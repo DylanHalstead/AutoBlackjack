12df4fdd8bac0a5993295d9f8c1aed2863f8a644
DylanHalstead <dylanhalstead11@gmail.com>
2023-12-09 17:04:02 -0500

    class utilization and refactor

--- card.py
+++ card.py
@@ -1,21 +1,25 @@
 class Card:
-    def __init__(self, card_string):
-      self.card_string = card_string
-      self.value_string = card_string[:-1]
-      self.value = self.get_value()
-      self.suit = card_string[-1]
-    
-    def get_value(self):
-      if self.value_string.isdigit():
-        return int(self.value_string)
-      elif self.value_string == 'A':
-        return 11
-      else:
-        return 10
-      
-    def set_ace_value(self, ace_value):
-      if self.value_str == 'A':
-          self.value = ace_value
-    
-    def __str__(self):
-      return f"Card: {self.card_string}, Value String: {self.value_string}, Value: {self.value}, Suit: {self.suit}"
+  def __init__(self, card_string, top_left, bottom_right):
+    self.card_string = card_string
+    self.value_string = card_string[:-1]
+    self.value = self.get_value()
+    self.suit = card_string[-1]
+    self.top_left = {'x': 0, 'y': 0}
+    self.bottom_right = {'x': 0, 'y': 0}
+    self.top_left = top_left
+    self.bottom_right = bottom_right
+  
+  def get_value(self):
+    if self.value_string.isdigit():
+      return int(self.value_string)
+    elif self.value_string == 'A':
+      return 11
+    else:
+      return 10
+
+  def set_ace_value(self, ace_value):
+    if self.value_str == 'A':
+      self.value = ace_value
+  
+  def __str__(self):
+    return f"Card: {self.card_string}, Value String: {self.value_string}, Value: {self.value}, Suit: {self.suit}"
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import cv2
 from ultralytics import YOLO
 import numpy as np
+from card import Card
+from hand import Hand
 
 model = YOLO('runs/detect/train7/weights/best.pt')
 
@@ -17,7 +19,7 @@ while capture.isOpened():
     detect_xyxy = result.xyxy.tolist() if result.xyxy is not None else []
     card_positions = [box for box in detect_xyxy]
 
-    # Dictionary to store bounding box coordinates by class name
+    # Store Cards in a dictionairy with their bounding boxes
     class_bbox_dict = {}
     for c, bbox in zip(result.cls, detect_xyxy):
       class_id = int(c)
@@ -25,16 +27,15 @@ while capture.isOpened():
       if class_name not in class_bbox_dict:
         class_bbox_dict[class_name] = []
       
-      # Extracting bounding box coordinates
       x1, y1, x2, y2 = bbox
       top_left = {'x': x1, 'y': y1}
       bottom_right = {'x': x2, 'y': y2}
-      bbox_dict = {'top_left': top_left, 'bottom_right': bottom_right}
-      
-      # Appending the bbox dictionary to the list for the class
-      class_bbox_dict[class_name].append(bbox_dict)
+      # bbox_dict = {'top_left': top_left, 'bottom_right': bottom_right}
+      # class_bbox_dict[class_name].append(bbox_dict)
+      card = Card(class_name, top_left, bottom_right)
+      class_bbox_dict[class_name].append(card)
 
-    # Proximity threshold and grouping cards based on proximity
+    # Proximity threshold used for grouping cards
     proximity_threshold = 400
     grouped_hands = []
     assigned = set()
@@ -42,28 +43,29 @@ while capture.isOpened():
     for class_name, positions in class_bbox_dict.items():
       for i, card1 in enumerate(positions):
         if i not in assigned:
-          hand = [card1]
+          hand = Hand([card1])
           assigned.add(i)
 
+          # Create hands based on card proximity
           for j, card2 in enumerate(positions[i + 1:]):
-            # Calculate Euclidean distance between top-left coordinates
-            dist = np.linalg.norm(np.array(list(card1['top_left'].values())) - np.array(list(card2['top_left'].values())))
+            dist = np.linalg.norm(np.array(list(card1.top_left.values())) - np.array(list(card2.top_left.values())))
             if dist < proximity_threshold:
-              hand.append(card2)
+              hand.add_card(card2)
               assigned.add(j)
 
           grouped_hands.append(hand)
 
     # Drawing bounding boxes around grouped cards
     for hand in grouped_hands:
-      if len(hand) > 1:
-        x_coords = [card['top_left']['x'] for card in hand]
-        y_coords = [card['top_left']['y'] for card in hand]
+      cards = hand.cards
+      if len(cards) > 1:
+        x_coords = [card.top_left['x'] for card in cards]
+        y_coords = [card.top_left['y'] for card in cards]
 
         x_min = min(x_coords)
         y_min = min(y_coords)
-        x_max = max([card['bottom_right']['x'] for card in hand])
-        y_max = max([card['bottom_right']['y'] for card in hand])
+        x_max = max([card.bottom_right['x'] for card in cards])
+        y_max = max([card.bottom_right['y'] for card in cards])
 
         cv2.rectangle(annotated_frame, (int(x_min), int(y_min)), (int(x_max), int(y_max)), (0, 255, 0), 2)
 
